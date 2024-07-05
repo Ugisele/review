132c7f7a3784245cb630b13b3987bafee30f8620
Ugisele <Umutonigisele12@gmail.com>
2024-07-05 16:22:21 +0200

    styling

--- fashion/components/home.jsx
+++ fashion/components/home.jsx
@@ -15,41 +15,43 @@ export default function Home() {
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
-          
         }}
       >
-        <View style={{
-          paddingHorizontal: 20, 
-          paddingVertical: 20,
-          borderRadius: 10, 
-          marginBottom: 20, 
-          // flex:1,
-          display: 'grid',
-          justifyContent:'flex-end',
-          backgroundColor: 'rgba(0,0,0,0.5)',
-          width: "95%",
-          height: 300,
-          marginBottom: 10,
-        }}>
-        <Text
-          style={{ 
-            paddingVertical: 12, 
-            fontSize:28,
-            color: "white" }}
+        <View
+          style={{
+            paddingHorizontal: 20,
+            paddingVertical: 20,
+            borderRadius: 10,
+            marginBottom: 20,
+            // flex:1,
+            display: "grid",
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            width: "95%",
+            height: 300,
+            marginBottom: 10,
+          }}
         >
-          my G are you ready for the best view
-        </Text>
-        <Text 
-         style={{ 
-          paddingVertical: 2, 
-          paddingHorizontal: 14, 
-          color: "white" }}>
-          Click next to contuine
-        </Text>
-        <Button title="next"/>
-
+          <Text
+            style={{
+              paddingVertical: 12,
+              fontSize: 28,
+              color: "white",
+            }}
+          >
+            my G are you ready for the best view
+          </Text>
+          <Text
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 14,
+              color: "white",
+            }}
+          >
+            Click next to contuine
+          </Text>
+          <Button title="next" />
         </View>
-        
       </ImageBackground>
     </View>
   );
